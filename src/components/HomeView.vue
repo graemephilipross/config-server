102d229e39634302454408f66fb3214a69ff91ff
Graeme Ross <graeme@oneutilitybill.co>
2018-02-23 16:15:59 +0000

    Initial commit - scaffolding

---
+++ src/components/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <ul class="list">
+  </ul>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+  .list {
+    width: 100%;
+    padding: 0;
+  }
+</style>
---
+++ src/components/PostView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="waiting">
+    Not yet available
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+  .waiting {
+    padding: 10px;
+    color: #555;
+  }
+</style>
